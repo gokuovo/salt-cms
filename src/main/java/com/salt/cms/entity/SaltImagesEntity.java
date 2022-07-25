f4dc740068e33485fc06e94b676514bfe9d94fe7
Tumingjie <1019901646@qq.com>
2022-07-25 17:28:40 +0800

    实体类增加注释

--- src/main/java/com/salt/cms/entity/SaltImagesEntity.java
+++ src/main/java/com/salt/cms/entity/SaltImagesEntity.java
@@ -10,9 +10,15 @@ import lombok.experimental.Accessors;
 @Data
 @EqualsAndHashCode(callSuper = false)
 public class SaltImagesEntity {
+
     private String id;
+
+    //1:联系方式图标，2:首页背景动图，3:首页背景大图片，4首页背景小图片
     private String imageCode;
+
     private String imageUrl;
+
     private String noti;
+
     private String reserve;
 }
