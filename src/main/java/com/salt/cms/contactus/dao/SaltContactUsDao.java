b12a0f9f2cf70675a8dbfb4b6e5908d783b5c3ae
goku <389651824@qq.com>
2022-07-25 15:38:54 +0800

    contact us和侧边联系接口更新

--- src/main/java/com/salt/cms/contactus/dao/SaltContactUsDao.java
+++ src/main/java/com/salt/cms/contactus/dao/SaltContactUsDao.java
@@ -3,8 +3,11 @@ package com.salt.cms.contactus.dao;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.salt.cms.contactus.form.SaltContactForm;
 import com.salt.cms.entity.SaltContactEntity;
+import com.salt.cms.entity.SaltSocialEntity;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface SaltContactUsDao extends BaseMapper<SaltContactEntity> {
@@ -12,5 +15,11 @@ public interface SaltContactUsDao extends BaseMapper<SaltContactEntity> {
 
     @Select("select * from salt_contact")
     SaltContactForm getContact();
+
+    @Select("select * from salt_contact limit 6")
+    List<SaltSocialEntity> getSocialSix();
+
+    @Select("select * from salt_contact limit 4")
+    List<SaltSocialEntity> getSocialFour();
 
 }
---
+++ src/main/java/com/salt/cms/contactus/form/SaltSocialForm.java
@@ -0,0 +1,11 @@
+package com.salt.cms.contactus.form;
+
+import lombok.Data;
+
+@Data
+public class SaltSocialForm {
+    private String id;
+    private String connectCode;
+    private String imageUrl;
+    private String contactUrl;
+}
